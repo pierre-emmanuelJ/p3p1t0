186bf54f543a7ec21aec71637c5a7d8374912474
qwebify <quentin.maire@epitech.eu>
2016-05-13 19:07:58 +0200

    YOLOOOOO

---
+++ pepito/CMakeFiles/pepito.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pepito.dir/src/daemon.o"
+  "CMakeFiles/pepito.dir/src/main.o"
+  "CMakeFiles/pepito.dir/src/network.o"
+  "CMakeFiles/pepito.dir/src/utils.o"
+  "pepito.pdb"
+  "pepito"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pepito.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
